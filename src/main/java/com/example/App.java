0160276b9be2451438c1809dede522f63339459f
Vitalii Tymashkov <vitalii.tymashkov@gmail.com>
2022-08-18 21:21:45 +0300

    Add reduce countries method

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,8 +2,13 @@ package com.example;
 
 import com.example.service.Service;
 
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        new Service().reduceCountriesByLength();
+        List<String> data = List.of("30 99 used_ornaments", "30 105 max_capacity", "12 100 used_ornaments",
+                "20 80 used_ornaments", "12 120 max_capacity", "20 101 max_capacity", "21 110 used_ornaments");
+
+        System.out.println(new Service().go(data, 20));
     }
 }
--- src/main/java/com/example/service/Service.java
+++ src/main/java/com/example/service/Service.java
@@ -1,13 +1,105 @@
 package com.example.service;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
 
 public class Service {
-    public void reduceCountriesByLength() {
-        List<String> countries = Arrays.asList("Germany", "England", "China", "Denmark", "Brazil", "France", "Australia");
-        Optional<String> countryName = countries.stream().reduce((c1, c2) -> c1.length() > c2.length() ? c1 : c2);
-        countryName.ifPresent(System.out::println);
+    public List<String> go(List<String> data, int newOrnaments) {
+
+        Map <Integer, Value> dataSet = new HashMap<>();
+
+        for (String instruction: data) {
+
+            String[] instSet = instruction.split(" ");
+            int id = Integer.parseInt(instSet[0]);
+            int count = Integer.parseInt(instSet[1]);
+            String operation = instSet[2];
+            switch (operation) {
+                case "used_ornaments" -> {
+                    if (dataSet.containsKey(id)) {
+                        Value value = dataSet.get(id);
+                        value.setUsed(count);
+                        value.setRemSpace(value.getMaxCap() - count);
+                        dataSet.put(id, value);
+                    } else {
+                        Value value = new Value(count, null, null);
+                        dataSet.put(id, value);
+                    }
+
+                }
+                case "max_capacity" -> {
+                    if (dataSet.containsKey(id)) {
+                        Value value = dataSet.get(id);
+                        value.setMaxCap(count);
+                        value.setRemSpace(count - value.getUsed());
+                        dataSet.put(id, value);
+                    } else {
+                        Value value = new Value(null, count, null);
+                        dataSet.put(id, value);
+                    }
+                }
+                default -> System.out.println("Error"); //throw new RuntimeException("Wrong operation");
+            }
+        }
+
+        return dataSet.entrySet().stream()
+                .filter(i -> i.getValue().getRemSpace() != null)
+                .filter(i -> i.getValue().getRemSpace() <= newOrnaments)
+                .map(o -> o.getKey().toString()).sorted().toList();
+    }
+
+
+
+    class Value {
+        private Integer used;
+        private Integer maxCapacity;
+        private Integer remainingSpace;
+
+        public Value(Integer used, Integer maxCapacity, Integer remainingSpace) {
+            this.used = used;
+            this.maxCapacity = maxCapacity;
+            this.remainingSpace = remainingSpace;
+        }
+
+        public Integer getUsed() {
+            return isNull(used)? 0 : used;
+        }
+
+        public void setUsed(Integer used) {
+            this.used = used;
+        }
+
+        public Integer getMaxCap() {
+            return isNull(maxCapacity) ? 0: maxCapacity;
+        }
+
+        public void setMaxCap(Integer maxCap) {
+            this.maxCapacity = maxCap;
+        }
+
+        public Integer getRemSpace() {
+            return isNull(remainingSpace) ? null: remainingSpace;
+        }
+
+        public void setRemSpace(Integer remSpace) {
+            this.remainingSpace = remSpace;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Value value = (Value) o;
+            return Objects.equals(used, value.used) && Objects.equals(maxCapacity, value.maxCapacity) && Objects.equals(remainingSpace, value.remainingSpace);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(used, maxCapacity, remainingSpace);
+        }
     }
 }
